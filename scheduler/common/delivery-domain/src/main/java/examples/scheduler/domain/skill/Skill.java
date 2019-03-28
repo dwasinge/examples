c44016ae5fceda8ea8c2eba6e8d5002161e24e9e
Derek Wasinger <derekwasinger@gmail.com>
2019-03-27 19:53:06 -0700

    jpa changes for domain split

--- scheduler/common/delivery-domain/src/main/java/examples/scheduler/domain/skill/Skill.java
+++ scheduler/common/delivery-domain/src/main/java/examples/scheduler/domain/skill/Skill.java
@@ -3,10 +3,9 @@ package examples.scheduler.domain.skill;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-
-import org.springframework.data.annotation.Id;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
--- scheduler/microservices/delivery-skills-service/src/main/java/examples/scheduler/microservices/delivery/skills/Application.java
+++ scheduler/microservices/delivery-skills-service/src/main/java/examples/scheduler/microservices/delivery/skills/Application.java
@@ -2,8 +2,10 @@ package examples.scheduler.microservices.delivery.skills;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan( basePackages = {"examples.scheduler.domain.skill"} )
 public class Application {
 
     public static void main(String[] args) {
